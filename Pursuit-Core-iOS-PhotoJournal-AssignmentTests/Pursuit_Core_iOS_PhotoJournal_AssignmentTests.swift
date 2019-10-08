f00312eeb3a1fddc27d70a4cdcc3002728b8cbc6
Krystal Campbell <krystalcampbell@Krystals-MacBook-Pro.local>
2019-10-08 14:07:45 -0400

    added xcode project

---
+++ Pursuit-Core-iOS-PhotoJournal-AssignmentTests/Pursuit_Core_iOS_PhotoJournal_AssignmentTests.swift
@@ -0,0 +1,34 @@
+//
+//  Pursuit_Core_iOS_PhotoJournal_AssignmentTests.swift
+//  Pursuit-Core-iOS-PhotoJournal-AssignmentTests
+//
+//  Created by Krystal Campbell on 10/8/19.
+//  Copyright © 2019 Krystal Campbell. All rights reserved.
+//
+
+import XCTest
+@testable import Pursuit_Core_iOS_PhotoJournal_Assignment
+
+class Pursuit_Core_iOS_PhotoJournal_AssignmentTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
