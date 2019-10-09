15851b529cc43fd188923812891fadec30b13ab1
Krystal Campbell <krystalcampbell@Krystals-MacBook-Pro.local>
2019-10-09 15:33:39 -0400

    set up Action Sheet

--- Pursuit-Core-iOS-PhotoJournal-Assignment/Controllers/PhotoJournalEntryViewController.swift
+++ Pursuit-Core-iOS-PhotoJournal-Assignment/Controllers/PhotoJournalEntryViewController.swift
@@ -10,30 +10,31 @@ import UIKit
 
 class PhotoJournalEntryViewController: UIViewController, UICollectionViewDataSource{
     
-    var photosDeets = [Photos]() {
+    var photoDeets = [Photos]() {
         didSet {
             self.PhotosCollectionView.reloadData()
         }
     }
     private func loadData(){
         do {
-            try self.photosDeets = PhotosPersistenceManager.manager.getPhotos()
+            try self.photoDeets = PhotosPersistenceManager.manager.getPhotos()
         } catch {
             print(error)
         }
     }
 
      func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return photosDeets.count
+        return photoDeets.count
 
     }
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "photoCell", for: indexPath) as! photoCell
-        let photoDetails = photosDeets[indexPath.row]
+        let photoDetails = photoDeets[indexPath.row]
         cell.DescriptionLabel.text = photoDetails.title
         cell.DateLabel.text = photoDetails.creationDate
         cell.photosImageView.image = UIImage(data: photoDetails.image)
+        cell.delegate = self
         return cell 
     }
 
@@ -42,8 +43,8 @@ class PhotoJournalEntryViewController: UIViewController, UICollectionViewDataSou
         super.viewDidLoad()
         PhotosCollectionView.dataSource = self
         PhotosCollectionView.delegate = self
+        
         loadData()
-        //PhotosCollectionView.delegate = self
     }
     
     @IBOutlet weak var PhotosCollectionView: UICollectionView!
@@ -52,6 +53,7 @@ class PhotoJournalEntryViewController: UIViewController, UICollectionViewDataSou
         let storyBoard = UIStoryboard.init(name: "Main", bundle: nil)
         let AddPhotoVC = storyBoard.instantiateViewController(identifier: "AddPhotoJournalEntryVC") as! AddPhotoJournalEntryVC
         present(AddPhotoVC, animated: true, completion: nil)
+        AddPhotoVC.modalPresentationStyle = .currentContext
     }
     
 }
@@ -59,32 +61,39 @@ class PhotoJournalEntryViewController: UIViewController, UICollectionViewDataSou
 extension PhotoJournalEntryViewController: ActionSheets {
     //to create an action sheet, you need uialertcontrollers
     func actionSheet(tag: Int) {
-        let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
-        let delete = UIAlertAction(title: "Delete", style: .destructive) { (_) in
-            //Write code to delete
+        let actionSheet = UIAlertController(title: "Options", message: "Choose an Option", preferredStyle: .actionSheet)
+        let delete = UIAlertAction(title: "Delete", style: .destructive) { (action) in
+         // let   UIAlertAction
+            let photoSelected = self.photoDeets[tag]
+            self.photoDeets.remove(at: tag)
+            try? PhotosPersistenceManager.manager.deletePhotos(withID: photoSelected.creationDate)
+           
         }
-        let edit = UIAlertAction(title: "Edit", style: .default) { (_) in
-            //Write code to edit
+        let edit = UIAlertAction(title: "Edit", style: .default) { (action) in
+         //   let editPhoto = self.photoDeets[tag]
+           // try? PhotosPersistenceManager.manager.//Write code to edit
         }
-        let share = UIAlertAction(title: "Share", style: .default) { (_) in
-            // write code to share
+        let share = UIAlertAction(title: "Share", style: .default) { (action) in
+            let image = UIImage(data: self.photoDeets[tag].image)
+            let share = UIActivityViewController(activityItems: [image!], applicationActivities: [])
+            self.present(share, animated: true, completion: nil)
         }
-        let cancel = UIAlertAction(title: "Cancel", style: .cancel) { (_) in
-            // write code to cancel
-        }
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel)
+         // dismiss(animated: true, completion: nil)
+        
         actionSheet.addAction(edit)
         actionSheet.addAction(share)
         actionSheet.addAction(delete)
         actionSheet.addAction(cancel)
         
+        self.present(actionSheet, animated: true, completion: nil)
     }
-
-    
 }
+
 
 extension PhotoJournalEntryViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize.init(width: 300, height: 300)
+        return CGSize.init(width: 350, height: 350)
     }
 }
 
